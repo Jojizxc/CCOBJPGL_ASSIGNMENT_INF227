d08f510b9f9a0f26b776068b827c18abec54c9f2
Bianca Louise Rosales <119952034+Jojizxc@users.noreply.github.com>
2023-05-02 11:17:13 +0800

    Create App.java

---
+++ EXERCISE#2/App.java
@@ -0,0 +1,33 @@
+import Explorers.*;
+import Planets.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Planet mars = new Mars();
+        Planet saturn = new Saturn();
+        Planet mercury = new Mercury();
+        Planet venus = new Venus();
+        Planet morag = new Morag();
+
+
+        Explorer astronaut = new HumanExplorer();
+        Explorer rover = new RobotExplorer();
+        
+
+        mars.accept(astronaut);
+        saturn.accept(astronaut);
+        mercury.accept(astronaut);
+        venus.accept(astronaut);
+        morag.accept(astronaut);
+
+
+
+        mars.accept(rover);
+        saturn.accept(rover);
+        mercury.accept(rover);
+        venus.accept(rover);
+        morag.accept(rover);
+    }
+}
