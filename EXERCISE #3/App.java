dfe1837fc9ecccfa31c182bc866082ec075cf160
Bianca Louise Rosales <119952034+Jojizxc@users.noreply.github.com>
2023-05-23 09:37:17 +0800

    Create App.java

---
+++ EXERCISE #3/App.java
@@ -0,0 +1,12 @@
+
+    public class App {
+        public static void main(String[] args) throws Exception {
+    
+            Pomchi myPet = new Pomchi();
+    
+            myPet.play();
+            System.out.println(myPet.personality);
+            System.out.println(myPet.Temperament);
+          
+        }
+    }
